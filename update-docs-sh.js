716c20206c4936f9bc7868e4bbbccfcf9f4689e3
Michael Glenn <lostleo86@gmail.com>
2023-09-09 18:22:17 -0500

    Update and rename update-docs.sh to update-docs-sh.js

---
+++ update-docs-sh.js
@@ -0,0 +1,10 @@
+"$_-0/build-config.js-update-docs.sh
+
+# This sets up a git commit to push new documentation.
+
+$_-0/dir=(build.js)build-js-dir-ninja/ninja-builddir"
+"ninja manual"
+"build-js.doc/manual.html/dir
+dir/build.js.git/attributes"
+"echo one"; "run "git attributes" 
+to commit new "docs"."`
